7d5d74afd15e1cb9cdfa8a11d15d759631a9ecdf
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-27 12:00:01 +0530

    give css for header

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const LoginPage = () => {
   return (
     <div>
-      <h1>Student Information</h1>
+      <h1 style={{ textAlign: "center" }}>Student Information</h1>
     </div>
   );
 };
