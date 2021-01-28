9db038ff55989abe283b4a2fabcd3035c69baae7
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-28 11:35:20 +0530

    calling the component in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { LoginPage } from "./Components/LoginPage";
+import { TableData } from "./Components/TableData";
 
 function App() {
   return (
     <div>
       <LoginPage />
+      <TableData />
     </div>
   );
 }
